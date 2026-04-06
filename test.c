707a62d7e09901bdfbe6c5d0082bf96082beed55
xiaowu <wudashuaige001@gmail.com>
2026-04-06 23:02:10 +0800

    fuxi

--- test.c
+++ test.c
@@ -191,7 +191,7 @@ int main()
 
     int (*pf)(int ,int)=Add;
     //int sum=(*pf)(2,3);
-    int sum=(****pf)(2,3);
+    int sum=pf(2,3);
     printf("%d",sum);
 //测试使用qsort来排序结构体数据
 /*     int arr[10]={0,9,8,7,6,5,4,3,2,1};
