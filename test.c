686f3a789e1a54097424e092a330065491de883b
xiaowu <wudashuaige001@gmail.com>
2026-07-24 15:05:13 +0800

    update

--- test.c
+++ test.c
@@ -347,6 +347,26 @@ int is_left_rotate(char* arr1,char* arr2)
 
 int main()
 {
+
+
+    int n=0;
+    while(scanf("%d",&n)==1)
+    {
+        int i=0,j=0;
+        for(i=0;i<n;i++)
+        {
+            for(j=0;j<n;j++)
+            {
+                if(i==0 || j==0 ||i==n-1 ||j==n-1)
+                printf("* ");
+                else 
+                printf("  ");
+            }
+            printf("\n");
+        }
+    }
+
+
 //动态内存
 /*     int arr[10]={0};
     int* p=(int*)malloc(40);
